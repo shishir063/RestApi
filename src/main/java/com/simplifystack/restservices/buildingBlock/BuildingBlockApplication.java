de50a94907e8ad0152e85da47f312aaa68306c29
shishirp063 <shishir063@gmail.com>
2020-07-31 22:03:36 +0530

    First commit for rst api

---
+++ src/main/java/com/simplifystack/restservices/buildingBlock/BuildingBlockApplication.java
@@ -0,0 +1,13 @@
+package com.simplifystack.restservices.buildingBlock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingBlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingBlockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplifystack/restservices/buildingBlock/BuildingBlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplifystack.restservices.buildingBlock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingBlockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
